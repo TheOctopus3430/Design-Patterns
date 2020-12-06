06679f3d5d8e019f6ef9728c431dc712ba6bec72
TheOctopus3430 <zhangyu_techlife@163.com>
2020-12-06 15:44:17 +0800

    模板方法设计模式

---
+++ src/main/java/com/example/dp/behavioral/template/GameInterface.java
@@ -0,0 +1,22 @@
+package com.example.dp.behavioral.template;
+
+public interface GameInterface {
+
+    void initialize();
+
+    void startPlay();
+
+    void endPlay();
+
+    default void play() {
+
+        //初始化游戏
+        initialize();
+
+        //开始游戏
+        startPlay();
+
+        //结束游戏
+        endPlay();
+    }
+}
---
+++ src/main/java/com/example/dp/behavioral/template/Main.java
@@ -0,0 +1,12 @@
+package com.example.dp.behavioral.template;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Game football = new Football();
+        football.play();
+    }
+
+
+}
