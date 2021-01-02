c047731596878988794a9b36952feb6a4e59f7e5
TheOctopus3430 <zhangyu_techlife@163.com>
2021-01-02 17:17:15 +0800

    事件回调

--- src/main/java/com/example/callback/demo1/CallBackA.java
+++ src/main/java/com/example/callback/demo1/CallBackA.java
@@ -3,6 +3,6 @@ package com.example.callback.demo1;
 public class CallBackA implements CallBackInf {
     @Override
     public void call() {
-        System.out.println("execute callback method...");
+        System.out.println("execute CallBackA  callback method...");
     }
 }
